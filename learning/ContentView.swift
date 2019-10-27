c37afa3363fd49026b1202b983ed9d02763f3eab
vanacek <andrew.vanacek@gmail.com>
2019-10-26 23:27:41 -0500

    Sections 1-3

--- learning/ContentView.swift
+++ learning/ContentView.swift
@@ -10,7 +10,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        VStack (alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .lineLimit(nil)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+    .padding()
     }
 }
 
